9968a547009f6b7336fa68af53eb315ba6d0ac89
bobogogo1990 <954575656@qq.com>
2018-08-06 20:02:39 +0800

    抽离css到单独文件

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@
 const path = require('path');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const webpack = require('webpack');
 
 module.exports = {
@@ -18,17 +19,23 @@ module.exports = {
     rules: [
       { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
       { test: /\.vue$/, loader: "vue-loader" },
-      { test: /\.css$/, use: ["vue-style-loader", "css-loader"] },
+      { test: /\.css$/, use: [MiniCssExtractPlugin.loader, "css-loader"] },
       {
         test: /\.less$/, use: [
-          "vue-style-loader",
-          { loader: "css-loader", options: { importLoaders: 1 } },
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+
+            },
+          },
+          "css-loader",
           "postcss-loader",
           "less-loader"
         ]
       }
     ]
   },
+  devtool: 'inline-source-map',
   plugins: [
     new VueLoaderPlugin(),
     new HtmlWebpackPlugin({
@@ -37,7 +44,11 @@ module.exports = {
       template: path.resolve(__dirname, 'index.html'),
       inject: 'body',
     }),
-    new webpack.HashedModuleIdsPlugin()
+    new webpack.HashedModuleIdsPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[chunkhash].chunk.css',
+    }),
   ],
   resolve: {
     extensions: ['.js', '.vue', '.json'],
