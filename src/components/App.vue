86d98ad423138cf8437b8caf03a3bc1c5429ecb3
bobogogo1990 <954575656@qq.com>
2018-08-06 15:58:58 +0800

    新增vue支持

---
+++ src/components/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+    <div class="header">{{header}}</div>
+    <div class="main">
+      {{main}}
+      <div class="msg">
+        {{msg}}
+      </div>
+    </div>
+    <div class="footer">{{footer}}</div>
+  </div>
+</template>
+
+<script>
+import { getUserInfo } from "utils/common";
+export default {
+  name: "App",
+  data() {
+    return {
+      header: "HEADER",
+      main: "MAIN",
+      msg: "Loading...",
+      footer: "FOOTER"
+    };
+  },
+  async created() {
+    const r = await getUserInfo();
+    this.msg = r.data.userId;
+  }
+};
+</script>
+
+<style>
+.header {
+  background-color: #eee;
+  font-size: 40px;
+}
+</style>
